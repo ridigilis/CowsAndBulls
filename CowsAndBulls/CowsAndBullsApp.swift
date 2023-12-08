31d2e91c4ca8b14187cc2849acabcd94394416e6
ridigilis <rgroner2@gmail.com>
2023-12-08 07:53:27 -0600

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Ricky David Groner II on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
