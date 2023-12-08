a9592f438cee2561d42a6dda324e40027b0a39b4
ridigilis <rgroner2@gmail.com>
2023-12-08 08:19:52 -0600

    designing our user interface

--- CowsAndBulls/ContentView.swift
+++ CowsAndBulls/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let guesses = Array(repeating: "1234", count: 20)
+    
+    func submitGuess( ) {
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            HStack {
+                TextField("Enter a guess...", text: .constant("1234"))
+                Button("Go", action: submitGuess)
+            }
+            .padding()
+            
+            List(guesses, id: \.self) { guess in
+                HStack {
+                    Text(guess)
+                    Spacer()
+                    Text("4c 0b")
+                }
+            }
         }
-        .padding()
+        .frame(width: 250)
+        .frame(minHeight: 300)
     }
 }
 
